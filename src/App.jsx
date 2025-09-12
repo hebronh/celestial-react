2615734c3812ed4acca3891baf16325a7cd3f36d
Hebron Bekele <hebronhbekele@gmail.com>
2025-09-12 12:10:18 -0500

    wix contacts

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,15 @@ function WaitingPage() {
 
     try {
       // Example: Send to Formspree (replace with your endpoint)
-      await fetch("https://formspree.io/f/movnbwkk", {
-        method: "POST",
-        headers: { 
-          "Accept": "application/json",
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ email })
-      });
+     await fetch("https://celestialcamo.com/_functions/subscribe", {
+     method: "POST",
+     headers: { 
+      "Accept": "application/json", 
+      "Content-Type": "application/json"
+  },
+  body: JSON.stringify({ email })
+});
+
 
       setSubmitted(true);
       setEmail("");
